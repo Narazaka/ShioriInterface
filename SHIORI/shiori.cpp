dfffd938434b7b3be6791d960ae382954538e210
Narazaka <info@narazaka.net>
2020-12-30 22:28:14 +0900

    cpp impl

--- SHIORI/shiori.cpp
+++ SHIORI/shiori.cpp
@@ -7,67 +7,47 @@
  **/
 #include <stdlib.h>
 #include <string.h>
-#include "bool.h"
 #include "shiori.h"
 
-#include "cshiori.c"
-#include "shiori_events.c"
+#include <msclr/marshal_cppstd.h>
+using namespace msclr::interop;
 
-SHIORI_EXPORT bool SHIORI_CALL load(const MEMORY_HANDLE h, long len) {
+SHIORI_EXPORT BOOL SHIORI_CALL load(const MEMORY_HANDLE h, long len) {
 	char* str = (char*)malloc(sizeof(char) * (len + 1));
+	System::String^ csstr;
 	strncpy(str, (const char*)h, len);
 	str[len] = '\0';
 	SHIORI_FREE(h);
-	return cshiori_load(str, shiori_load);
+	bool ret;
+	csstr = marshal_as<System::String^>(str);
+	free(str);
+	ret = ShioriInterface::ShioriInterface::Load(csstr);
+	return ret;
 }
 
 #define SHIORI_LINES_BUFFER_STEP 10
 SHIORI_EXPORT MEMORY_HANDLE SHIORI_CALL request(const MEMORY_HANDLE h, long* len) {
 	char* str = (char*)malloc(sizeof(char) * (*len + 1));
-	char** lines;
-	char** lines_new;
-	size_t lines_index;
-	char* str_p = str;
-	char* str_p_new;
-	char* resstr;
+	const char* resstr;
 	MEMORY_HANDLE reth;
+	System::String^ csstr;
 	strncpy(str, (const char*)h, *len);
 	str[*len] = '\0';
 	SHIORI_FREE(h);
-	lines = NULL;
-	lines_index = 0;
-	while (true) {
-		char* line;
-		size_t line_length;
-		if (0 == (lines_index % SHIORI_LINES_BUFFER_STEP)) {
-			lines_new = (char**)realloc(lines, sizeof(char*) * (lines_index + SHIORI_LINES_BUFFER_STEP));
-			if (lines_new == NULL) {
-				size_t i;
-				for (i = 0; i < lines_index; ++i) {
-					free(*(lines + i));
-				}
-				free(lines);
-				return NULL;
-			}
-			lines = lines_new;
-		}
-		str_p_new = strstr(str_p, "\r\n");
-		if (str_p_new == NULL) break;
-		line_length = str_p_new - str_p;
-		line = (char*)malloc(sizeof(char) * (line_length + 1));
-		strncpy(line, str_p, line_length);
-		line[line_length] = '\0';
-		*(lines + lines_index) = line;
-		str_p = str_p_new + 2;
-		lines_index++;
+	csstr = marshal_as<System::String^>(str);
+	free(str);
+	csstr = ShioriInterface::ShioriInterface::Request(csstr);
+	{
+		marshal_context ctx;
+		resstr = ctx.marshal_as<const char*>(csstr);
+		*len = strlen(resstr);
+		reth = (MEMORY_HANDLE)SHIORI_MALLOC(*len);
+		memcpy(reth, resstr, *len);
 	}
-	resstr = cshiori_request(lines, lines_index, shiori_request);
-	*len = strlen(resstr);
-	reth = (MEMORY_HANDLE)SHIORI_MALLOC(*len);
-	memcpy(reth, resstr, *len);
 	return reth;
 }
 
-SHIORI_EXPORT bool SHIORI_CALL unload(void) {
-	return cshiori_unload(shiori_unload);
+SHIORI_EXPORT BOOL SHIORI_CALL unload(void) {
+	return ShioriInterface::ShioriInterface::Unload();
 }
+
